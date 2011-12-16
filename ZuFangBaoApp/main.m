f362a861c1e37b975fbcbd9668c05c61b346a0ce
wang yongjun <wangyongjun@wangmatoMac-mini.local>
2011-12-16 10:02:02 +0800

    Initial Commit

---
+++ ZuFangBaoApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ZuFangBaoApp
+//
+//  Created by yongjun wang on 11-12-16.
+//  Copyright www.zufangbao.com 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
